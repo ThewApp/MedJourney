64759e56fc13323b502fbd78ee88cc40eb2fb702
ThewBear <26767607+ThewBear@users.noreply.github.com>
2020-01-01 01:35:51 +0700

    [gatsby] Load firebase in onRenderBody

--- gatsby/gatsby-ssr.js
+++ gatsby/gatsby-ssr.js
@@ -4,4 +4,15 @@
  * See: https://www.gatsbyjs.org/docs/ssr-apis/
  */
 
-// You can delete this file if you're not using it
+import React from "react";
+
+export const onRenderBody = ({ setPostBodyComponents }) => {
+  setPostBodyComponents([
+    <script src="/__/firebase/7.6.1/firebase-app.js"></script>,
+    <script src="/__/firebase/7.6.1/firebase-analytics.js"></script>,
+    <script src="/__/firebase/7.6.1/firebase-auth.js"></script>,
+    <script src="/__/firebase/7.6.1/firebase-firestore.js"></script>,
+    <script src="/__/firebase/7.6.1/firebase-functions.js"></script>,
+    <script src="/__/firebase/init.js"></script>
+  ]);
+};
