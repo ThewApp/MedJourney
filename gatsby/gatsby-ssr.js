5d355f8ff54b3539f0bec79f1d0bc505b9c49801
ThewBear <26767607+ThewBear@users.noreply.github.com>
2020-01-01 18:59:48 +0700

    [gatsby] Add key prop to firebase libraries

--- gatsby/gatsby-ssr.js
+++ gatsby/gatsby-ssr.js
@@ -8,11 +8,11 @@ import React from "react";
 
 export const onRenderBody = ({ setPostBodyComponents }) => {
   setPostBodyComponents([
-    <script src="/__/firebase/7.6.1/firebase-app.js"></script>,
-    <script src="/__/firebase/7.6.1/firebase-analytics.js"></script>,
-    <script src="/__/firebase/7.6.1/firebase-auth.js"></script>,
-    <script src="/__/firebase/7.6.1/firebase-firestore.js"></script>,
-    <script src="/__/firebase/7.6.1/firebase-functions.js"></script>,
-    <script src="/__/firebase/init.js"></script>
+    <script key="firebase-app" src="/__/firebase/7.6.1/firebase-app.js"></script>,
+    <script key="firebase-analytics" src="/__/firebase/7.6.1/firebase-analytics.js"></script>,
+    <script key="firebase-auth" src="/__/firebase/7.6.1/firebase-auth.js"></script>,
+    <script key="firebase-firestore" src="/__/firebase/7.6.1/firebase-firestore.js"></script>,
+    <script key="firebase-functions" src="/__/firebase/7.6.1/firebase-functions.js"></script>,
+    <script key="firebase-init" src="/__/firebase/init.js"></script>
   ]);
 };
