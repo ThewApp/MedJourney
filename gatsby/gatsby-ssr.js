f39d09d0fa0658090cc5efa6f43fd6fd318d5809
ThewBear <26767607+ThewBear@users.noreply.github.com>
2020-01-01 19:05:21 +0700

    [gatsby] fix prettier

--- gatsby/gatsby-ssr.js
+++ gatsby/gatsby-ssr.js
@@ -8,11 +8,26 @@ import React from "react";
 
 export const onRenderBody = ({ setPostBodyComponents }) => {
   setPostBodyComponents([
-    <script key="firebase-app" src="/__/firebase/7.6.1/firebase-app.js"></script>,
-    <script key="firebase-analytics" src="/__/firebase/7.6.1/firebase-analytics.js"></script>,
-    <script key="firebase-auth" src="/__/firebase/7.6.1/firebase-auth.js"></script>,
-    <script key="firebase-firestore" src="/__/firebase/7.6.1/firebase-firestore.js"></script>,
-    <script key="firebase-functions" src="/__/firebase/7.6.1/firebase-functions.js"></script>,
+    <script
+      key="firebase-app"
+      src="/__/firebase/7.6.1/firebase-app.js"
+    ></script>,
+    <script
+      key="firebase-analytics"
+      src="/__/firebase/7.6.1/firebase-analytics.js"
+    ></script>,
+    <script
+      key="firebase-auth"
+      src="/__/firebase/7.6.1/firebase-auth.js"
+    ></script>,
+    <script
+      key="firebase-firestore"
+      src="/__/firebase/7.6.1/firebase-firestore.js"
+    ></script>,
+    <script
+      key="firebase-functions"
+      src="/__/firebase/7.6.1/firebase-functions.js"
+    ></script>,
     <script key="firebase-init" src="/__/firebase/init.js"></script>
   ]);
 };
