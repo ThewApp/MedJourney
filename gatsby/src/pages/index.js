59b033c1dd34267378c2fef502a0a2afef6613e1
ThewBear <26767607+ThewBear@users.noreply.github.com>
2020-01-07 22:26:56 +0700

    [gatsby] Update design & 404.html

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -2,17 +2,19 @@ import React from "react";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
+import Cover from "../components/cover";
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
+    <Cover />
     <div className="container mx-auto flex flex-col sm:flex-row items-center justify-center my-6">
-      <p className="max-w-sm mx-4 mb-4">
+      <p className="max-w-sm mx-4 my-6">
         กิจกรรมสุดพิเศษ 2 วันเต็ม ที่จะทำให้ทุก ๆ คนได้รู้ลึก รู้จริง
         รู้ทุกเรื่องเกี่ยวกับคณะแพทย์ จุฬาฯ รับรองว่าหาจากที่ไหนไม่ได้อีกแน่นอน
       </p>
-      <div className="max-w-sm rounded overflow-hidden shadow-md mx-4">
-        <div className="px-6 py-2 md:py-4">
+      <div className="max-w-sm rounded overflow-hidden shadow-md mx-4 my-6">
+        <div className="px-6 py-6">
           <div className="font-bold text-xl mb-1">วันที่</div>
           <p className="text-gray-700">
             <span role="img" aria-label="Spiral Calendar">
@@ -21,7 +23,7 @@ const IndexPage = () => (
             21 - 22 มีนาคม 2563
           </p>
         </div>
-        <div className="px-6 py-2 md:py-4">
+        <div className="px-6 py-6">
           <div className="font-bold text-xl mb-1">สถานที่</div>
           <p className="text-gray-700">
             <span role="img" aria-label="Map Pin">
