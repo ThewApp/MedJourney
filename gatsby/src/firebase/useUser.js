7f2a46c5a8d54656c06a9c9ef5ca0b9b1bab4c41
ThewBear <26767607+ThewBear@users.noreply.github.com>
2020-01-12 16:37:53 +0700

    [gatsby] Add qrcode

---
+++ gatsby/src/firebase/useUser.js
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import { navigate } from "gatsby";
+
+import useAuth from "./useAuth";
+import useFirestore from "./useFirestore";
+
+export default function() {
+  const auth = useAuth();
+  const firestore = useFirestore();
+  const [authUser, setAuthUser] = useState();
+  const [firestoreUser, setFirestoreUser] = useState();
+  useEffect(() => {
+    if (auth) {
+      return auth().onAuthStateChanged(user => {
+        if (user) {
+          setAuthUser(user);
+        } else {
+          navigate(`/login`, { replace: true });
+        }
+      });
+    }
+  }, [auth]);
+
+  useEffect(() => {
+    if (authUser) {
+      firestore()
+        .doc(`users/${authUser.uid}`)
+        .get()
+        .then(docSnapshot => {
+          if (!docSnapshot.get("firstName")) {
+            navigate("/register", { replace: true });
+          }
+          setFirestoreUser(docSnapshot.data());
+        });
+    } else {
+      setFirestoreUser();
+    }
+  }, [authUser]);
+
+  return [authUser, firestoreUser];
+}
--- gatsby/src/pages/profile.js
+++ gatsby/src/pages/profile.js
@@ -1,16 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import QRCode from "qrcode";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
-import Cover from "../components/cover";
+import useUser from "../firebase/useUser";
 
-const IndexPage = () => {
+const ProfilePage = () => {
+  const [authUser, firestoreUser] = useUser();
+  const [qrcodeUrl, setqrcodeUrl] = useState();
+
+  useEffect(() => {
+    if (firestoreUser && firestoreUser.shortId) {
+      QRCode.toDataURL(
+        firestoreUser.shortId,
+        { margin: 2, scale: 50 },
+        (err, url) => {
+          setqrcodeUrl(url);
+        }
+      );
+    }
+  }, [firestoreUser]);
+
   return (
     <Layout>
       <SEO title="Profile" />
-      <Cover />
+      {firestoreUser ? (
+        <div className="container mx-auto">
+          <img alt="QR Code" aria-describedby="shortId" className="w-1/2 sm:w-1/3 md:w-1/4 mx-auto" src={qrcodeUrl} />
+          <p id="shortId" className="text-center">{firestoreUser.shortId}</p>
+        </div>
+      ) : (
+        "Loading"
+      )}
     </Layout>
   );
 };
 
-export default IndexPage;
+export default ProfilePage;
