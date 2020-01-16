3c70f535f8bfe7e9b6d2d80fd7138809500a808f
ThewBear <26767607+ThewBear@users.noreply.github.com>
2020-01-16 20:24:58 +0700

    [gatsby] Remove firebase config prefetch

--- gatsby/gatsby-ssr.js
+++ gatsby/gatsby-ssr.js
@@ -9,18 +9,6 @@ import React from "react";
 import { LocationProvider } from "./src/context/location";
 import { UserProvider } from "./src/context/user";
 
-export const onRenderBody = ({ setHeadComponents, setPostBodyComponents }) => {
-  setHeadComponents([
-    <link
-      key="firebaseinit"
-      rel="preload"
-      href="/__/firebase/init.json"
-      as="fetch"
-      crossOrigin="anonymous"
-    ></link>
-  ]);
-};
-
 export const wrapPageElement = ({ element }) => (
   <LocationProvider>{element}</LocationProvider>
 );
