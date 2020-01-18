489acd701ffa5b59e7fd2b95fd673917fccc52c9
ThewBear <26767607+ThewBear@users.noreply.github.com>
2020-01-18 15:06:39 +0700

    [gatsby] Add manual stamp

--- gatsby/src/pages/staff.js
+++ gatsby/src/pages/staff.js
@@ -9,6 +9,7 @@ const StaffPage = ({ location }) => {
   const firestore = useFirestore();
   const [staff, setStaff] = useState();
   const [userShortId, setUserShortId] = useState();
+  const [manualShortId, setManualShortId] = useState("");
   const [status, setStatus] = useState("Ready");
   const [stampedUserShortId, setStampedUserShortId] = useState();
 
@@ -30,6 +31,8 @@ const StaffPage = ({ location }) => {
       firestore &&
       staff &&
       userShortId &&
+      userShortId.length === 8 &&
+      !manualShortId &&
       status === "Ready" &&
       stampedUserShortId !== userShortId
     ) {
@@ -52,12 +55,23 @@ const StaffPage = ({ location }) => {
             setStatus("An error has occured.");
           }
         );
-    } else if (stampedUserShortId && stampedUserShortId === userShortId) {
+    } else if (
+      stampedUserShortId &&
+      (stampedUserShortId === userShortId ||
+        stampedUserShortId === manualShortId)
+    ) {
       setStatus("Stamped");
     } else if (status === "Stamped") {
       setStatus("Ready");
     }
-  }, [firestore, staff, userShortId, status, stampedUserShortId]);
+  }, [
+    firestore,
+    staff,
+    userShortId,
+    manualShortId,
+    status,
+    stampedUserShortId
+  ]);
 
   useEffect(() => {
     if (status === "Success") {
@@ -66,16 +80,69 @@ const StaffPage = ({ location }) => {
     }
   }, [status]);
 
+  function manualStamp() {
+    if (
+      firestore &&
+      staff &&
+      manualShortId &&
+      manualShortId.length === 8 &&
+      status === "Ready" &&
+      stampedUserShortId !== manualShortId
+    ) {
+      setStatus("Manual Stamping");
+      firestore()
+        .collection("stamps")
+        .add({
+          eventType: staff.eventType,
+          staffId: staff.staffId,
+          userShortId: manualShortId,
+          timestamp: firestore.FieldValue.serverTimestamp()
+        })
+        .then(
+          () => {
+            setStatus("Success");
+            setStampedUserShortId(manualShortId);
+            setManualShortId("");
+          },
+          error => {
+            console.error(error);
+            setStatus("An error has occured.");
+          }
+        );
+    }
+  }
+
   return (
     <Layout>
       <SEO title="App" />
       {staff && <h1 className="text-center text-lg">{staff.eventType}</h1>}
       {staff ? (
-        <div className="container mx-auto md:flex">
-          <QRCodeReader className="md:w-1/2 md:mx-2" setCode={setUserShortId} />
-          <div className="md:mx-2 md:flex-grow md:order-first">
-            <p>Code: {userShortId}</p>
-            <p>Status: {status}</p>
+        <div className="container mx-auto sm:flex">
+          <QRCodeReader className="sm:w-1/2 sm:mx-2" setCode={setUserShortId} />
+          <div className="sm:mx-2 sm:flex-auto flex justify-between sm:flex-col sm:order-first">
+            <div className="p-4 text-lg sm:text-2xl">
+              <p>User ID: {manualShortId || userShortId}</p>
+              <p>Status: {status}</p>
+            </div>
+            <div className="p-4 w-1/2 sm:w-auto">
+              <label className="block mb-2" htmlFor="userShortId">
+                รหัส 8 หลัก
+              </label>
+              <input
+                className="block max-w-full shadow appearance-none bg-white border py-2 px-3 mb-2"
+                id="userShortId"
+                name="userShortId"
+                inputMode="numeric"
+                value={manualShortId}
+                onChange={event => setManualShortId(event.target.value)}
+              />
+              <button
+                className="bg-primary-100 hover:bg-primary-200 py-2 px-4 rounded"
+                onClick={manualStamp}
+              >
+                Manual Stamp
+              </button>
+            </div>
           </div>
         </div>
       ) : (
