57b972a3950861dc1432718767fb4dea0c1e8c97
ThewBear <26767607+ThewBear@users.noreply.github.com>
2020-02-03 21:49:17 +0700

    [gatsby] Update events page

--- gatsby/src/pages/events.js
+++ gatsby/src/pages/events.js
@@ -7,18 +7,46 @@ import SEO from "../components/seo";
 
 function EventCard({ event, className }) {
   return (
-    <Link
-      to={`/events/${event.eventPath}`}
-      key={event.id}
-      className={"block p-4 shadow-md rounded-sm " + className}
-    >
-      <h3 className="sm:text-lg text-primary-600 font-medium mb-2">
+    <div className={"p-4 shadow-md rounded-sm flex flex-col " + className}>
+      <h3 className="text-lg text-primary-800 font-medium mb-3">
         {event.eventName}
       </h3>
-      <ReactMarkdown className="text-sm md:text-base markdown text-gray-800">
+      <ReactMarkdown className="markdown text-gray-800 mb-3">
         {event.eventShortDescription}
       </ReactMarkdown>
-    </Link>
+      <div className="mt-auto">
+        {event.roundInfo && (
+          <div className="text-secondary-700 mb-3">
+            <p>กิจกรรมนี้มีการเปิดเป็นรอบ</p>
+            <span className="inline-flex items-center">
+              <svg
+                className="fill-current mr-2 h-4 inline"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 97.16 97.16"
+              >
+                <path d="M48.58 0C21.793 0 0 21.793 0 48.58s21.793 48.58 48.58 48.58 48.58-21.793 48.58-48.58S75.367 0 48.58 0zm0 86.823c-21.087 0-38.244-17.155-38.244-38.243S27.493 10.337 48.58 10.337 86.824 27.492 86.824 48.58 69.667 86.823 48.58 86.823z" />
+                <path d="M73.898 47.08H52.066V20.83a4 4 0 00-8 0v30.25a4 4 0 004 4h25.832a4 4 0 000-8z" />
+              </svg>
+              รอบละ {event.roundInfo.duration} นาที
+            </span>
+          </div>
+        )}
+        <Link
+          to={`/events/${event.eventPath}`}
+          key={event.id}
+          className="text-primary-600 flex items-center justify-end"
+        >
+          รายละเอียด
+          <svg
+            className="fill-current ml-2 h-3 inline"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 268.8 268.8"
+          >
+            <path d="M265 126l-80-80a13 13 0 00-18 17l59 59H13a13 13 0 000 25h213l-59 59a12 12 0 1018 17l80-80c5-5 5-13 0-17z" />
+          </svg>
+        </Link>
+      </div>
+    </div>
   );
 }
 
@@ -37,9 +65,12 @@ export default ({ data, location }) => {
           </p>
         </div>
       </div>
-      <div className="flex flex-wrap justify-around container mx-auto">
+      <div className="flex flex-wrap justify-center">
         {data.allEventsYaml.nodes.map(node => (
-          <EventCard className="w-full md:w-5/12 m-3 sm:m-6" event={node} />
+          <EventCard
+            className="w-full sm:w-5/12 max-w-xl mx-2 my-3 sm:m-6"
+            event={node}
+          />
         ))}
       </div>
     </Layout>
@@ -54,6 +85,9 @@ export const query = graphql`
         eventName
         eventPath
         eventShortDescription
+        roundInfo {
+          duration
+        }
       }
     }
   }
